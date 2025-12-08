42b66bb3a50a9df4b7c4c010377dc49fcfe2c7b2
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-12-08 08:18:10 +0200

    fibonacci recursive progress

--- Task_2/fiboRecursive.js
+++ Task_2/fiboRecursive.js
@@ -19,20 +19,83 @@
 
 
 
-
+// let prev = 0;
+// let next;
+// let curr = 1;
 // let n = 16;
-function fib ( n) {
-    if (n === 0) return;
-    let prev = 0;
-let next;
-let curr = 1;
-     next = prev + curr
-      prev = curr
-      curr = next
-    console.log(curr + " ");
+// let arr = [];
+// function fib ( n, prev, curr ) {
+    
+//     if (n === 0) return;
+//      next = prev + curr
+//       prev = curr
+//       curr = next
+//         arr.push(curr);
+//      let sequence = arr.join('+');
+//     console.log(sequence, `= ${n}`);
 
-    fib(n-1);
+//     fib(n-1, prev, curr);
+// }
+
+// fib(n, prev, curr);
+
+// let prev = 0;
+// let curr = 1;
+// let n = 34;
+// let arr = [];  
+
+// function fib(n, prev, curr) {
+//   if (n === 0) {
+
+//     let sequence = arr.join(" + ");
+//     let total = arr.reduce((sum, num) => sum + num, 0);
+//     console.log(sequence + " = " + total);
+//     return;
+//   }
+
+  
+//   let next = prev + curr;
+
+//   arr.push(next);
+
+  
+//   fib(n - 1, curr, next);
+// }
+
+// fib(n, prev, curr);
+
+function fiboRecursive(n, prev = 0, curr = 1, sequence = []) {
+ 
+  if (n === 0){
+    return 0
+  };
+  if (n === 1) {
+    return 1
+  };
+
+  let next = prev + curr;
+
+ 
+  if (next > n) {
+    
+    let stringNum = sequence.join(" + ");
+    return `${stringNum} = ${n}`;
+  }
+
+ 
+  sequence.push(next);
+
+
+  if (next === n) {
+    let stringNum = sequence.join(" + ");
+    return `${stringNum} = ${n}`;
+  }
+
+  
+  return fiboRecursive(n, curr, next, sequence);
 }
 
-fib(16, );
 
+
+let result = fiboRecursive(34);
+console.log(result);
