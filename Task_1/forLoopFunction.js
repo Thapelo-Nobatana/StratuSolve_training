82d4297e01642f145a75588ff24574147090cbe0
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-12-11 07:42:05 +0200

    feat: implement addAll() using a standard for loop to sum array values

--- Task_1/forLoopFunction.js
+++ Task_1/forLoopFunction.js
@@ -2,20 +2,22 @@
 
 function addAll(array) {
     let totalSum = 0;
-     let partsNumbers = [];
-
-    for (let i = 0; i < array.length; i++) {
-        totalSum += array[i];
-
+    
+  
+    for (let i = 0; i <= 6; i++ ) {
+    
+     
+      const currentSum = array.reduce((acc, val) => acc + val, 0);
+       totalSum += currentSum;
+       
+       array.pop();
     }
-    for (let i = totalSum; i > 0; i--) {
-        partsNumbers.push(i);    
-    }
-    let totalNumers = partsNumbers.join('+');
 
-    return `${totalNumers} = ${totalSum}`;
+
+
+    return totalSum;
 }
-let array = [1, 1, 1, 1, 1];
 
-console.log(addAll(array)); //  "5+4+3+2+1= 5"
+let array = [1, 1, 1, 1, 1, 6];
+
 
